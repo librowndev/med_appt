b9e932fea01e32828d233cef2063b459d061eb70
librowndev <librown@vt.edu>
2026-05-30 12:54:17 -0400

    add mongo connection

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MgEdxTMjrT0Zt6hzGOpt16sX>@172.21.21.84:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
